83702b87fc2aa18bfa21fc5348533fb042bbfb4b
davasafgi <63136493+davasafgi@users.noreply.github.com>
2020-04-20 22:27:48 +0700

    Tambahaan Dava

--- Toko_hedon/Toko_hedon.py
+++ Toko_hedon/Toko_hedon.py
@@ -7,20 +7,31 @@ import daftar_laptop
 def uang_masuk(uang):
     if (uang >= 1000000) and (uang <= 5000000):
         print ("Selamat anda bisa membeli barang dengan spec low,mari saya tunjukkan")
-        print ("1.acer swift air 3")
-        print ("2. ASUS APALAH ITU ")
+        print ("1.Acer swift air 3")
+        print ("2.Asus VivoBook Max ")
+        print ("3.HP 14-af118AU")
         #nanti ini diisi list laptop low
     elif (uang >= 5000100) and (uang <= 10000000):
+        print ('1.Asus X509FJ')
+        print ('2.HP ProBook 430 G5')
+        print ('3.Acer Nitro 5 AN515-52')
         return print ("Selamat anda bisa membeli barang dengan spec medium,mari saya tunjukkan")
     elif (uang >= 10000100) and (uang <= 20000000):
+        print ('1.Asus ROG Zephyrus G GA502DU')
+        print ('2.Acer Predator Triton 300')
+        print ('3.Lenovo Legion Y7000 SE')
         return print ("Selamat anda bisa membeli barang dengan spec high,mari saya tunjukkan")
     elif (uang >= 100000) and (uang <= 999999):
+        print ('1.Logitech G243 Prodigy Gaming Keyboard')
+        print ('2.Asus ROG Strix Impact II Gaming Mouse')
+        print ('3.Cooler Master-MasterPulse MH750 Gaming Headset')        
         return print ("Mohon maaf anda tidak bisa membeli laptop dengan harga segitu,Tapi anda bisa membeli aksesorisnya")
 
 
 
+
 kembalian = 0
-a=float(input("Masukkan uang yang ingin anda keluarkan="))
+a=int(input("Masukkan uang yang ingin anda keluarkan="))
 while True:
     if (kembalian != 0):
         uang_masuk(kembalian)
--- Toko_hedon/daftar_laptop.py
+++ Toko_hedon/daftar_laptop.py
@@ -1,49 +1,65 @@
 def laptop_low(laptop):
     if (laptop == 1):
-        return print ("Anda memilih acer swift air 3 (Rp.3.500.000)")
+        return print ("1.Acer swift air 3 (Rp.3.500.000)")
     elif (laptop == 2):
-        return print ("Anda memilih ASUS Saja")
+        return print ("2.Asus VivoBook Max (Rp.3.549.000)")
+    elif (laptop == 3):
+        return print ("3.HP 14-af118AU (Rp.3.985.000)")
      
 def laptop_medium(laptop):
     if (laptop == 1):
-        return print ("Anda memilih acer swift air 3 (Rp.6.000.000)")
+        return print ("1.Asus X509FJ (Rp.8.699.000)")
     elif (laptop == 2):
-        return print ("Anda memilih ASUS Saja")
+        return print ("2.HP ProBook 430 G5 (Rp.8.250.000)")
+    elif (laptop == 3):
+        return print ("3.Acer Nitro 5 AN515-52 (Rp.9.338.000)")
 
 def laptop_high(laptop):
     if (laptop == 1):
-        return print ("Anda memilih acer swift air 3 (Rp.6.000.000)")
+        return print ("1.Asus ROG Zephyrus G GA502DU (Rp.19.495.000)")
     elif (laptop == 2):
-        return print ("Anda memilih ASUS Saja")
+        return print ("2.Acer Predator Triton 300 (Rp.19.499.000)")
+    elif (laptop == 3):
+        return print ("3.Lenovo Legion Y7000 SE (Rp.16.999.000)")
 
 def aksesoris(acc):
-    if (laptop == 1):
-        return print ("Anda memilih acer swift air 3 (Rp.6.000.000)")
-    elif (laptop == 2):
-        return print ("Anda memilih ASUS Saja")
+    if (acc == 1):
+        return print ("1.Logitech G243 Prodigy Gaming Keyboard (Rp.698.500)")
+    elif (acc == 2):
+        return print ("Asus ROG Strix Impact II Gaming Mouse (Rp.620.000)")
+    elif (acc == 3):
+        return print ("Cooler Master-MasterPulse MH750 Gaming Headset (Rp.988.000)")
 
 
     #daftar laptop ini nyesuain sama di list depan
 def harga_laptop_low(b):
     switcher={
                 1:3500000,
-                2:10,
-                3:22,
+                2:3549000,
+                3:3985000,
              }
     return switcher.get(b,"Anda salah input")
 
 def harga_laptop_medium(b):
     switcher={
-                1:6000000,
-                2:10,
-                3:22,
+                1:8699000,
+                2:8250000,
+                3:9338000,
              }
     return switcher.get(b,"Anda salah input")
     
-def harga_aksesoris(b):
+def harga_laptop_high(b):
     switcher={
-                1:6000000,
-                2:10,
-                3:22,
+                1:19495000,
+                2:19499000,
+                3:16999000,
+             }
+    return switcher.get(b,"Anda salah input")
+
+def harga_aksesoris(b):
+    switcher={ 
+                1:698500,
+                2:620000,
+                3:988000,
              }
     return switcher.get(b,"Anda salah input")
