326767c17a073eea672081b32296d69513d5cb4a
Dime07 <dimasbkt7@gmail.com>
2020-04-21 15:02:47 +0700

    program akhir

--- Toko_hedon/Toko_hedon.py
+++ Toko_hedon/Toko_hedon.py
@@ -7,31 +7,53 @@ import daftar_laptop
 def uang_masuk(uang):
     if (uang >= 1000000) and (uang <= 5000000):
         print ("Selamat anda bisa membeli barang dengan spec low,mari saya tunjukkan")
-        print ("1.Acer swift air 3")
-        print ("2.Asus VivoBook Max ")
-        print ("3.HP 14-af118AU")
+        print ("1.Acer Aspire V5-132 (Rp.3.500.000)")
+        print ("2.Asus VivoBook Max (Rp.3.549.000)")
+        print ("3.HP 14-af118AU (Rp.3.985.000)")
+        print ("4.Lenovo ThinkPad 11e (Rp.2.198.000)")
+        print ("5.Acer Aspire One 722 (Rp.2.500.000)")
+        print ("6.HP 14-bw016au (Rp.4.025.000)")
+        print ("7.Lenovo Ideapad B470 (Rp 4.599.000)")
+        print ("8.Lenovo Yoga A12 (Rp 4.999.000)")
         #nanti ini diisi list laptop low
     elif (uang >= 5000100) and (uang <= 10000000):
-        print ('1.Asus X509FJ')
-        print ('2.HP ProBook 430 G5')
-        print ('3.Acer Nitro 5 AN515-52')
-        return print ("Selamat anda bisa membeli barang dengan spec medium,mari saya tunjukkan")
-    elif (uang >= 10000100) and (uang <= 20000000):
-        print ('1.Asus ROG Zephyrus G GA502DU')
-        print ('2.Acer Predator Triton 300')
-        print ('3.Lenovo Legion Y7000 SE')
-        return print ("Selamat anda bisa membeli barang dengan spec high,mari saya tunjukkan")
+        print ("Selamat anda bisa membeli barang dengan spec medium,mari saya tunjukkan")
+        print ('1.Asus X509FJ (Rp.8.699.000)')
+        print ('2.HP ProBook 430 G5 (Rp.8.250.000)')
+        print ('3.Acer Nitro 5 AN515-52 (Rp.9.338.000)')
+        print ("4.ASUS X401U-WX099D (Rp 5.249.000)")
+        print ("5.Lenovo Ideapad 330S 15 (Rp 6.415.000)")
+        print ("6.Lenovo Yoga 520 14 (Rp 7.500.000)")
+        print ("7.Acer Aspire 5 A514-52G (Rp 5.549.000)")
+        print ("8.TOSHIBA Satellite L740-1219U (Rp.6.310.400")
+    elif (uang >= 10000100) and (uang <= 50000000):
+        print ("Selamat anda bisa membeli barang dengan spec high,mari saya tunjukkan")
+        print ('1.Asus ROG Zephyrus G GA502DU (Rp.19.495.000)')
+        print ('2.Acer Predator Triton 300 (Rp.19.499.000)')
+        print ('3.Lenovo Legion Y7000 SE (Rp.16.999.000)')
+        print ("4.Lenovo Legion Y530 (Rp 10.799.000	)")
+        print ("5.Acer Spin 5 SP513-52N-36P7 (Rp 11.420.000)")
+        print ("6.Lenovo ThinkPad Yoga 12 (Rp 13.099.000)")
+        print ("7.ASUS ROG Strix GL503 (Rp 14.299.000)")
+        print ("8.Apple MacBook Air 11.6 (Rp 15.266.000)")
+        print ("9.Razer Blade Stealth QHD (Rp 18.325.000)")
+        print ("10.ASUS ZenBook Flip S UX370UA (Rp 17.350.000)")
     elif (uang >= 100000) and (uang <= 999999):
-        print ('1.Logitech G243 Prodigy Gaming Keyboard')
-        print ('2.Asus ROG Strix Impact II Gaming Mouse')
-        print ('3.Cooler Master-MasterPulse MH750 Gaming Headset')        
-        return print ("Mohon maaf anda tidak bisa membeli laptop dengan harga segitu,Tapi anda bisa membeli aksesorisnya")
+        print ("Mohon maaf anda tidak bisa membeli laptop dengan harga segitu,Tapi anda bisa membeli aksesorisnya")
+        print ('1.Logitech G243 Prodigy Gaming Keyboard (Rp.698.500)')
+        print ('2.Asus ROG Strix Impact II Gaming Mouse (Rp.620.000)')
+        print ('3.Cooler Master-MasterPulse MH750 Gaming Headset (Rp.988.000)')   
+        print ("4.HP DeskJet Ink Advantage 2135 All-in-One Printer (Rp.825.000)")
+        print ("5.JBL T450 Foldable Wired On-Ear Headphones (Rp.235.000)")
+        print ("6.Portable 1080 P Home Theater Mini LED Proyektor (Rp.527.000)")
+        print ("7.Mouse Wireless Logitech M238  (Rp.146.000)")
+        print ("8.Fantech mk871 keyboard (Rp.440.000)")
 
 
 
 
 kembalian = 0
-a=int(input("Masukkan uang yang ingin anda keluarkan="))
+a=int(input("Masukkan uang yang ingin anda keluarkan=Rp."))
 while True:
     if (kembalian != 0):
         uang_masuk(kembalian)
@@ -50,9 +72,9 @@ while True:
         harga=daftar_laptop.harga_laptop_medium(b)
         kembalian=a-harga
         print ("sisa uang=Rp.",kembalian)
-    elif (a >= 10000100) and (a <= 20000000):
+    elif (a >= 10000100) and (a <= 50000000):
         r=daftar_laptop.laptop_high(b)
-        harga=daftar_laptop.harga_laptop_medium(b)
+        harga=daftar_laptop.harga_laptop_high(b)
         kembalian=a-harga
         print ("sisa uang=Rp.",kembalian)
 
@@ -62,10 +84,11 @@ while True:
         kembalian=a-harga
         print ("sisa uang=Rp.",kembalian)
 
-    kata=input("apakah anda ingin belanja lagi?")
+    kata=input("mau belanja lagi gak?")
+    print (" ")
     if (kata == "tidak"):
         break
-
+print ("terima kasih telah menghabiskan uang ditoko kami")
 
 
 
--- Toko_hedon/daftar_laptop.py
+++ Toko_hedon/daftar_laptop.py
@@ -1,34 +1,80 @@
 def laptop_low(laptop):
     if (laptop == 1):
-        return print ("1.Acer swift air 3 (Rp.3.500.000)")
+        return print ("Terima kasih telah membeli Acer Aspire V5-132")
     elif (laptop == 2):
-        return print ("2.Asus VivoBook Max (Rp.3.549.000)")
+        return print ("Terima kasih telah membeli Asus VivoBook Max")
     elif (laptop == 3):
-        return print ("3.HP 14-af118AU (Rp.3.985.000)")
+        return print ("Terima kasih telah membeli HP 14-af118AU")
+    elif (laptop == 4):
+        return print ("Terima kasih telah membeli Lenovo ThinkPad 11e")
+    elif (laptop == 5):
+        return print ("Terima kasih telah membeli Acer Aspire One 722")
+    elif (laptop == 6):
+        return print ("Terima kasih telah membeli HP 14-bw016au")
+    elif (laptop == 7):
+        return print ("Terima kasih telah membeli Lenovo Ideapad B470")
+    elif (laptop == 8):
+        return print ("Terima kasih telah membeli Lenovo Yoga A12")
+
+
      
 def laptop_medium(laptop):
     if (laptop == 1):
-        return print ("1.Asus X509FJ (Rp.8.699.000)")
+        return print ("Terima kasih telah membeli Asus X509FJ")
     elif (laptop == 2):
-        return print ("2.HP ProBook 430 G5 (Rp.8.250.000)")
+        return print ("Terima kasih telah membeli HP ProBook 430 G5")
     elif (laptop == 3):
-        return print ("3.Acer Nitro 5 AN515-52 (Rp.9.338.000)")
+        return print ("Terima kasih telah membeli Acer Nitro 5 AN515-52")
+    elif (laptop == 4):
+        return print ("Terima kasih telah membeli ASUS X401U-WX099D")
+    elif (laptop == 5):
+        return print ("Terima kasih telah membeli Lenovo Ideapad 330S 15")
+    elif (laptop == 6):
+        return print ("Terima kasih telah membeli Lenovo Yoga 520 14")
+    elif (laptop == 7):
+        return print ("Terima kasih telah membeli Acer Aspire 5 A514-52G")
+    elif (laptop == 8):
+        return print ("Terima kasih telah membeli TOSHIBA Satellite L740-1219U")
 
 def laptop_high(laptop):
     if (laptop == 1):
-        return print ("1.Asus ROG Zephyrus G GA502DU (Rp.19.495.000)")
+        return print ("Terima kasih telah membeli Asus ROG Zephyrus G GA502DU")
     elif (laptop == 2):
-        return print ("2.Acer Predator Triton 300 (Rp.19.499.000)")
+        return print ("Terima kasih telah membeli Acer Predator Triton 300")
     elif (laptop == 3):
-        return print ("3.Lenovo Legion Y7000 SE (Rp.16.999.000)")
+        return print ("Terima kasih telah membeli Lenovo Legion Y7000 SE")
+    elif (laptop == 4):
+        return print ("Terima kasih telah membeli Lenovo Legion Y530")
+    elif (laptop == 5):
+        return print ("Terima kasih telah membeli Acer Spin 5 SP513-52N-36P7")
+    elif (laptop == 6):
+        return print ("Terima kasih telah membeli Lenovo ThinkPad Yoga 12")
+    elif (laptop == 7):
+        return print ("Terima kasih telah membeli ASUS ROG Strix GL503")
+    elif (laptop == 8):
+        return print ("Terima kasih telah membeli Apple MacBook Air 11.6")
+    elif (laptop == 9):
+        return print ("Terima kasih telah membeli Razer Blade Stealth QHD")
+    elif (laptop == 10):
+        return print ("Terima kasih telah membeli ASUS ZenBook Flip S UX370UA")
 
 def aksesoris(acc):
     if (acc == 1):
-        return print ("1.Logitech G243 Prodigy Gaming Keyboard (Rp.698.500)")
+        return print ("Terima kasih telah membeli Logitech G243 Prodigy Gaming Keyboard")
     elif (acc == 2):
-        return print ("Asus ROG Strix Impact II Gaming Mouse (Rp.620.000)")
+        return print ("Terima kasih telah membeli Asus ROG Strix Impact II Gaming Mouse")
     elif (acc == 3):
-        return print ("Cooler Master-MasterPulse MH750 Gaming Headset (Rp.988.000)")
+        return print ("Terima kasih telah membeli Cooler Master-MasterPulse MH750 Gaming Headset")
+    elif (acc == 4):
+        return print ("Terima kasih telah membeli HP DeskJet Ink Advantage 2135 All-in-One Printer")
+    elif (acc == 5):
+        return print ("Terima kasih telah membeli JBL T450 Foldable Wired On-Ear Headphones")
+    elif (acc == 6):
+        return print ("Terima kasih telah membeli Portable 1080 P Home Theater Mini LED Proyektor")
+    elif (acc == 7):
+        return print ("Terima kasih telah membeli Mouse Wireless Logitech M238")
+    elif (acc == 8):
+        return print ("Terima kasih telah membeli Fantech mk871 keyboard")
 
 
     #daftar laptop ini nyesuain sama di list depan
@@ -37,6 +83,12 @@ def harga_laptop_low(b):
                 1:3500000,
                 2:3549000,
                 3:3985000,
+                4:2198000,
+                5:2500000,
+                6:4025000,
+                7:4599000,
+                8:4999000,
+
              }
     return switcher.get(b,"Anda salah input")
 
@@ -45,6 +97,12 @@ def harga_laptop_medium(b):
                 1:8699000,
                 2:8250000,
                 3:9338000,
+                4:5249000,
+                5:6415000,
+                6:7500000,
+                7:5549000,
+                8:6310400,
+
              }
     return switcher.get(b,"Anda salah input")
     
@@ -53,6 +111,13 @@ def harga_laptop_high(b):
                 1:19495000,
                 2:19499000,
                 3:16999000,
+                4:10799000,
+                5:11420000,
+                6:13099000,
+                7:14299000,
+                8:15266000,
+                9:18325000,
+                10:17350000,
              }
     return switcher.get(b,"Anda salah input")
 
@@ -61,5 +126,10 @@ def harga_aksesoris(b):
                 1:698500,
                 2:620000,
                 3:988000,
+                4:825000,
+                5:235000,
+                6:527000,
+                7:146000,
+                8:440000,
              }
     return switcher.get(b,"Anda salah input")
